ccfe5008fa3585f41c95ab7e8d2699cdcd3b0df6
Lisina Yulia <aglaya_yulia@mail.ru>
2024-06-27 16:47:15 +0300

    add ask_account_card

---
+++ main.py
@@ -0,0 +1,6 @@
+from src.widget import mask_account_card
+
+print(mask_account_card("Счет 73654108430135874305"))
+print(mask_account_card("Visa Platinum 7000 7922 8960 6361"))
+print(mask_account_card("Maestro 7000 7922 8960 6361"))
+
