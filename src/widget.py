0b9c6742184616b36e0384362194bceb92b003ca
Lisina Yulia <aglaya_yulia@mail.ru>
2024-06-27 16:04:41 +0300

    remove processing and fiks readme

--- src/widget.py
+++ src/widget.py
@@ -1,8 +1,20 @@
-def mask_account_card(card_account_type_and_card_number: str) -> str:
+def mask_account_card(card_account_type_and_card_number: str) -> str:   # card_account_type_and_card_number - тип карты/счета и номер карты/
     """Функция общей маскировки карты и счета"""
 
 
-# card_account_type_and_card_number - тип карты/счета и номер карты/
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def get_data(input_info: str) -> str:
@@ -11,8 +23,7 @@ def get_data(input_info: str) -> str:
     cut_date = input_info[:10].split("-")
     return ".".join(cut_date[::-1])
 
-Напишите функцию, которая принимает на вход строку вида
-2018-07-11T02:26:18.671407
- и возвращает строку с датой в виде
-11.07.2018
-.
+# Напишите функцию, которая принимает на вход строку вида
+# 2018-07-11T02:26:18.671407
+#  и возвращает строку с датой в виде
+# 11.07.2018.
