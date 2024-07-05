7bd2c80eea2fdc5b412e1daf295bdacc55725816
Lisina Yulia <aglaya_yulia@mail.ru>
2024-07-05 08:47:02 +0300

    add README.md, main/py

--- main.py
+++ main.py
@@ -4,4 +4,3 @@ from src.widget import mask_account_card, get_data
 # print(mask_account_card("Visa Platinum 7000 7922 8960 6361"))
 # print(mask_account_card("Maestro 7000 7922 8960 6361"))
 print(get_data("2018-07-11T02:26:18.671407"))
-
